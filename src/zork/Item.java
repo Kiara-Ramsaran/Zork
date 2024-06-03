308102dafca4f9ae5e203cd744521f4ebb75b4fc
Kiara-Ramsaran <144252856+Kiara-Ramsaran@users.noreply.github.com>
2024-06-03 01:05:13 -0400

    Completed comments for item.java

--- src/zork/Item.java
+++ src/zork/Item.java
@@ -1,57 +1,79 @@
 package zork;
 
+/* This class represents a single item in the game. */
 public class Item extends OpenableObject {
-  private int weight;
-  private String name;
-  private String description;
-  private boolean isOpenable;
-  private Inventory inventory;
+  private int weight;             // Weight of the item.
+  private String name;            // Name of the item.
+  private String description;     // Description of the item.
+  private boolean isOpenable;     // Can the item be opened.
+  private Inventory inventory;    // Other items held within this item.
   
-
+  /* Constructor */
   public Item(int weight, String name, boolean isOpenable, String description) {
     this.weight = weight;
     this.name = name;
     this.isOpenable = isOpenable;
     this.description = description;
+    /* If the item has the capacity to hold extra items internally, set up an inventory. */
     if (isOpenable)
     inventory = new Inventory(100);
   }
 
+  /* Attempts to open the object. If the item cannot be opened a message
+   * will be printed. If the item can be opened, the 'isOpen' status
+   * will be set to true. 
+   */
   public void open() {
     if (!isOpenable)
+    {
       System.out.println("The " + name + " cannot be opened.");
-
+    }
+    else
+    {
+      this.setOpen(true);
+    }
   }
 
+  /* Returns the weight of the item. */
   public int getWeight() {
     return weight;
   }
 
+  /* Sets the weight of the item. */
   public void setWeight(int weight) {
     this.weight = weight;
   }
 
+  /* Returns the name of the item. */
   public String getName() {
     return name;
   }
 
+  /* Sets the name of the item. */
   public void setName(String name) {
     this.name = name;
   }
 
+  /* Determines if this is an openable item. 
+   * Returns true, if this is an openable item.
+   * Returns false, otherwise.
+  */
   public boolean isOpenable() {
     return isOpenable;
   }
 
+  /* Sets if the item is capable of being opened. */
   public void setOpenable(boolean isOpenable) {
     this.isOpenable = isOpenable;
   }
 
+  /* Returns the description of the item. */
   public String getDescription()
   {
     return this.description;
   }
 
+  /* Inserts an item into the present item. */
   public boolean addItem(Item item) {
     if (isOpenable)
       return inventory.addItem(item);
@@ -59,6 +81,7 @@ public class Item extends OpenableObject {
       return false;
 }
 
+/* Removes an item from the current item. */
 public Item removeItem(Item item){
   if(isOpenable)
     return inventory.removeItem(item);
@@ -68,9 +91,10 @@ public Item removeItem(Item item){
   return null;
 }
 
+/* Can be used for testing to print Item variables. */ 
 //public String toString()
 //{
-//  return "ITEM- " + this.name + " " + this.description + " " + this.weight + " " + this.isOpenable;
+//  return "ITEM- " + this.name + " " + this.description + " " + this.weight + " " + this.isOpenable + " " + this.isLocked() + " " + this.getKeyId() + " " + this.isOpen();
 //}
  
 }
