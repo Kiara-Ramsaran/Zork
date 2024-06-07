6bb592e95158952bf0832131db26d44ab589a34c
Kiara-Ramsaran <144252856+Kiara-Ramsaran@users.noreply.github.com>
2024-06-07 15:42:44 -0400

    Update Game.java

--- src/zork/Game.java
+++ src/zork/Game.java
@@ -20,7 +20,7 @@ public class Game {
 
   private Parser parser;
   private Room currentRoom;
-  Inventory playerInventory = new Inventory(10);
+  private Inventory playerInventory = new Inventory(10);
 
   /**
    * Create the game and initialise its internal map.
